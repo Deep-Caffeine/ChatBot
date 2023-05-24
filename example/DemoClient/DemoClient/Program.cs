e3a00c7a43b6b3a179f7946e3df73c6ebc862d9f
Cardroid <number1dh@gmail.com>
2023-05-24 16:04:56 +0900

    [Feat] Demo 클라이언트 추가

---
+++ example/DemoClient/DemoClient/Program.cs
@@ -0,0 +1,28 @@
+﻿using NetMQ;
+using NetMQ.Sockets;
+
+namespace DemoClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var client = new RequestSocket())
+            {
+                client.Connect("tcp://127.0.0.1:5555");
+
+                while (true)
+                {
+                    string input = Console.ReadLine();
+                    if (input == "exit")
+                        break;
+
+                    client.SendFrame(input);
+
+                    var msg = client.ReceiveFrameString();
+                    Console.WriteLine($"From Server: {msg}");
+                }
+            }
+        }
+    }
+}
